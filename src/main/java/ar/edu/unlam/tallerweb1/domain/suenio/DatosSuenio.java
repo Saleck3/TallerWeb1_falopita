69c34ffdfdf8a9d1abe291880098fe30dfca0522
Pablo Maitini <pmaitini@alumno.unlam.edu.ar>
2022-09-29 21:48:30 -0300

    se agrega get y post de prueba

---
+++ src/main/java/ar/edu/unlam/tallerweb1/domain/suenio/DatosSuenio.java
@@ -0,0 +1,13 @@
+package ar.edu.unlam.tallerweb1.domain.suenio;
+
+public class DatosSuenio {
+    public int getEdadObtenida() {
+        return edadObtenida;
+    }
+
+    public void setEdadObtenida(int edadObtenida) {
+        this.edadObtenida = edadObtenida;
+    }
+
+    private int edadObtenida;
+}

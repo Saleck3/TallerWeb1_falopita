d34af796a9441e7fc6cf04c4df60bba1bd2129e1
Lucas David Cardozo <lucas.david.car99@gmail.com>
2022-11-02 21:40:38 -0300

    Nuevos metodos para listar por estado o fecha

--- src/main/java/ar/edu/unlam/tallerweb1/infrastructure/recordatorio/RepositorioFechaRecordatorio.java
+++ src/main/java/ar/edu/unlam/tallerweb1/infrastructure/recordatorio/RepositorioFechaRecordatorio.java
@@ -1,10 +1,16 @@
 package ar.edu.unlam.tallerweb1.infrastructure.recordatorio;
 
 import ar.edu.unlam.tallerweb1.domain.recordatorio.FechaRecordatorio;
+import ar.edu.unlam.tallerweb1.domain.recordatorio.FechaRecordatorio.EstadosFecha;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface RepositorioFechaRecordatorio {
     FechaRecordatorio obtener(Long id);
     FechaRecordatorio guardar(FechaRecordatorio fecha);
     void eliminar(FechaRecordatorio fecha);
     FechaRecordatorio modificar(FechaRecordatorio fecha);
+    List<FechaRecordatorio> listarPorFecha(LocalDate fechaFiltro);
+    List<FechaRecordatorio> listarPorEstado(EstadosFecha estado);
 }
--- src/main/java/ar/edu/unlam/tallerweb1/infrastructure/recordatorio/RepositorioFechaRecordatorioImpl.java
+++ src/main/java/ar/edu/unlam/tallerweb1/infrastructure/recordatorio/RepositorioFechaRecordatorioImpl.java
@@ -1,10 +1,16 @@
 package ar.edu.unlam.tallerweb1.infrastructure.recordatorio;
 
 import ar.edu.unlam.tallerweb1.domain.recordatorio.FechaRecordatorio;
+import ar.edu.unlam.tallerweb1.domain.recordatorio.Recordatorio;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository("repositorioFechaRecordatorio")
 public class RepositorioFechaRecordatorioImpl implements RepositorioFechaRecordatorio{
@@ -37,5 +43,21 @@ public class RepositorioFechaRecordatorioImpl implements RepositorioFechaRecorda
         session = sessionFactory.getCurrentSession();
         session.update(fecha);
         return fecha;
+    }
+
+    @Override
+    public List<FechaRecordatorio> listarPorFecha(LocalDate fechaFiltro) {
+        session = sessionFactory.getCurrentSession();
+        Criteria cr = session.createCriteria(FechaRecordatorio.class);
+        cr.add(Restrictions.eq("fecha", fechaFiltro));
+        return cr.list();
+    }
+
+    @Override
+    public List<FechaRecordatorio> listarPorEstado(FechaRecordatorio.EstadosFecha estado) {
+        session = sessionFactory.getCurrentSession();
+        Criteria cr = session.createCriteria(FechaRecordatorio.class);
+        cr.add(Restrictions.eq("estado", estado));
+        return cr.list();
     }
 }
